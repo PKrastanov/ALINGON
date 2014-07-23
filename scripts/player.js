c4b853bb1ca8b5222a13aebd7bbe6780b666f1ce
Sayshoo <aleka98@abv.bg>
2014-07-23 21:16:31 +0300

    Update player.js

--- scripts/player.js
+++ scripts/player.js
@@ -1,13 +1,44 @@
-var Player = (function () {
+function player(name, color) {                       
+    this.pawns = init();
+    this.color = color
+    this.name = name;
 
-    var Player = function (name) {
-        this._name = name;
-    };
+    this.rollTheDice = function () {               // roll the dice functios it returns an array of the dices (2)
+       var dices = [];
+       dices[0] = random(2,6);        
+       dices[1] = random(2, 6);
+       return dices;
+    }
+   
+    this.addNewPawn = function(){                  // addNewPawn adds new Pawn to the Field 
+        for (var i = 0; i < this.pawns.length; i++) {
+            if (!(this.pawns[i].started || this.pawns[i].finished)) {
+                this.pawns[i].started = true;
+                break;
+            }
+        }
+    }
 
-    Player.prototype.getName = function () {
-        return this._name;
-    };
+    this.movePawn = function (pawnNumber, distance) {          //it takes the number of the pawn and the position to move 
+        if (this.pawns[pawnNumber].started && !this.pawns[pawnNumber].finished) {
+            this.pawns[pawnNumber].position += distance;
+        }
+    }
 
-    return Player;
 
-})();
+
+    function random(min, max) {
+        return Math.floor((Math.random() * max-1) + min);
+    }
+
+    function init() {
+        var pawns = [];
+        for (var i = 0; i < 4; i++) {
+            pawns[i] = new pawn(color);
+        }
+        return pawns;
+    }
+}
+
+
+
