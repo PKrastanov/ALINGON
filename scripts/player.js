b17c973c689248e47c810c70c2db0d7fe02f9d3f
Sayshoo <aleka98@abv.bg>
2014-07-24 11:01:33 +0300

    Update player.js

--- scripts/player.js
+++ scripts/player.js
@@ -2,13 +2,6 @@ function player(name, color) {
     this.pawns = init();
     this.color = color
     this.name = name;
-
-    this.rollTheDice = function () {    //it returns an array of the dices 
-       var dices = [];
-       dices[0] = random(2,6);        
-       dices[1] = random(2, 6);
-       return dices;
-    }
    
     this.addNewPawn = function(){                  // addNewPawn adds new Pawn to the Field 
         for (var i = 0; i < this.pawns.length; i++) {
@@ -23,12 +16,6 @@ function player(name, color) {
         if (this.pawns[pawnNumber].started && !this.pawns[pawnNumber].finished) {
             this.pawns[pawnNumber].position += distance;
         }
-    }
-
-
-
-    function random(min, max) {
-        return Math.floor((Math.random() * max-1) + min);
     }
 
     function init() {
