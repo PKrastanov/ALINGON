6e1af7c334234e8bf0fce9422e83bb906c808c6e
krustanov <pesho1994@gmail.com>
2014-07-24 11:16:52 +0300

    player fixed

--- scripts/pawn.js
+++ scripts/pawn.js
@@ -1,5 +1,11 @@
-function pawn() {          // started means that the pawn is on the board                    
-    this.started = false; 
-    this.finished = false;
-    this.position = 0;
-}
+var Pawn = (function () {
+
+    var Pawn = function () {          // started means that the pawn is on the board
+        this.started = false;
+        this.finished = false;
+        this.position = 0;
+    };
+
+    return Pawn;
+
+})();
--- scripts/player.js
+++ scripts/player.js
@@ -1,32 +1,6 @@
-function player(name, color) {                       
-    this.pawns = init();
-    this.color = color
-    this.name = name;
+var Player = (function () {
 
-    this.rollTheDice = function () {    //it returns an array of the dices 
-       var dices = [];
-       dices[0] = random(2,6);        
-       dices[1] = random(2, 6);
-       return dices;
-    }
-   
-    this.addNewPawn = function(){                  // addNewPawn adds new Pawn to the Field 
-        for (var i = 0; i < this.pawns.length; i++) {
-            if (!(this.pawns[i].started || this.pawns[i].finished)) {
-                this.pawns[i].started = true;
-                break;
-            }
-        }
-    }
-
-    this.movePawn = function (pawnNumber, distance) {          //it takes the number of the pawn and the position to move 
-        if (this.pawns[pawnNumber].started && !this.pawns[pawnNumber].finished) {
-            this.pawns[pawnNumber].position += distance;
-        }
-    }
-
-
-
+    //Private
     function random(min, max) {
         return Math.floor((Math.random() * max-1) + min);
     }
@@ -34,11 +8,43 @@ function player(name, color) {
     function init() {
         var pawns = [];
         for (var i = 0; i < 4; i++) {
-            pawns[i] = new pawn(color);
+            pawns[i] = new Pawn(color);
         }
         return pawns;
     }
-}
+
+    //Public
+    var Player = function(name, color) {
+        this.pawns = init();
+        this.color = color;
+        this.name = name;
+    };
+
+    Player.prototype.rollTheDice = function () {    //it returns an array of the dices
+       var dices = [];
+       dices[0] = random(2,6);        
+       dices[1] = random(2, 6);
+       return dices;
+    };
+
+    Player.prototype.addNewPawn = function(){                  // addNewPawn adds new Pawn to the Field
+        for (var i = 0; i < this.pawns.length; i++) {
+            if (!(this.pawns[i].started || this.pawns[i].finished)) {
+                this.pawns[i].started = true;
+                break;
+            }
+        }
+    };
+
+    Player.prototype.movePawn = function (pawnNumber, distance) {          //it takes the number of the pawn and the position to move
+        if (this.pawns[pawnNumber].started && !this.pawns[pawnNumber].finished) {
+            this.pawns[pawnNumber].position += distance;
+        }
+    };
+
+    return Player;
+
+})();
 
 
 
