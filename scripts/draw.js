7c33839bcdec464c33f04068eb1b6d78970340cb
kpetkova <kristina.petkova@hqs-it.com>
2014-07-24 10:06:00 +0300

    Show something small
    Това е само началото, тествам кирилицата (:

--- scripts/draw.js
+++ scripts/draw.js
@@ -1,5 +1,11 @@
 var Draw = (function () {
-
+ var my_canvas = document.getElementById("canvas");
+ var context = my_canvas.getContext("2d");
+ context.strokeRect(0, 0, 500, 500);
+ 
+ context.fillStyle = "green";
+ context.font = "30px Calibri";
+ context.fillText("This is our game board", 15, 175);
 
 
 })();
