f93565adc0f311e0c408045b06341208b8c0b830
krustanov <pesho1994@gmail.com>
2014-07-25 15:22:58 +0300

    engine main structore

--- scripts/draw.js
+++ scripts/draw.js
@@ -2,7 +2,7 @@ var Draw = (function () {
  var my_canvas = document.getElementById("canvas");
  var context = my_canvas.getContext("2d");
  context.strokeRect(0, 0, 500, 500);
- 
+
  context.fillStyle = "green";
  context.font = "30px Calibri";
  context.fillText("This is our game board", 15, 175);
--- scripts/engine.js
+++ scripts/engine.js
@@ -1,8 +1,50 @@
 var Start = (function () {
+'use strict';
 
-    var game = function() {
-        //TODO: Main function that will start the game.
+    var game = function(playerName, language) {
+
+        var allPlayers = init(playerName, language);
+
+        var play = function () {
+
+            if(isBotTurn()) {
+                //TODO: how bot will play (some animation).
+            }
+
+            //Draw Parts (every time).
+            //Draw.pawns(allPlayers);
+            //Draw.dice();
+
+            //Ask to trow the dice
+            //GameLogic.diceThrow();
+
+            //Get pawn or move pawn.
+
+            //Is Player win.
+
+            //Game over (will be in if).
+
+            //Repeat.
+        }
+
     };
+
+    function init (playerName, language) {
+        Draw.bord();
+
+        // TODO: add event listener for questions.
+
+        return {
+            gamer: new Player(playerName, language),
+            botOne: new Player('Bot1', getDifferentPlayer()),
+            botTwo: new Player('Bot2', getDifferentPlayer()),
+            botThree: new Player('Bot3', getDifferentPlayer())
+        };
+    }
+
+    function getDifferentPlayer (language) {
+        //TODO: Get bots for the game.
+    }
 
     return {
         game: game
